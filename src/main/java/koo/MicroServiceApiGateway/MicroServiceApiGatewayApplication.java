c6f85780e24fd2c85a2bfa971ea7fee5e251b178
rngustj9139 <mnb9139@khu.ac.kr>
2022-09-20 02:47:21 +0900

    Setting first environment

---
+++ src/main/java/koo/MicroServiceApiGateway/MicroServiceApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package koo.MicroServiceApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/koo/MicroServiceApiGateway/MicroServiceApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package koo.MicroServiceApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
