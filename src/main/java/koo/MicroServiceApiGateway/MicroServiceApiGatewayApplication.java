c08fc4bb5ebf84b2fc9c53b68158588f5bb931fb
rngustj9139 <mnb9139@khu.ac.kr>
2022-11-25 12:34:10 +0900

    Update application.yml, bootstrap.yml

--- src/main/java/koo/MicroServiceApiGateway/MicroServiceApiGatewayApplication.java
+++ src/main/java/koo/MicroServiceApiGateway/MicroServiceApiGatewayApplication.java
@@ -1,13 +1,21 @@
 package koo.MicroServiceApiGateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MicroServiceApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServiceApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public HttpTraceRepository httpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
 	}
 
 }
